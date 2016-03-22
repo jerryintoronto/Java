78467a66c7f1cd31c4cac55f6016a3f7ba040478
jerryintoronto <jerry.kong@hotmail.ca>
2016-03-21 22:20:58 -0400

    java

---
+++ GUI/drawString.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class drawString extends JPanel {
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        myPanel p = new myPanel();
+        f.add(p);
+        f.setSize(800, 600);
+        f.setVisible(true);
+    }
+
+
+    static class myPanel extends JPanel {
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            setBackground(Color.WHITE);
+
+            int x = 20, y = 20;
+            g.drawString("HELLO", x, y);
+
+            String output = "How are you?";
+
+            for (int i = 1; i < 7; i++) {
+                g.setColor(new Color(0, i * 30 , i * 30));
+                g.setFont(new Font("Courier", Font.BOLD, 22 + 3*i));
+                g.drawString(output, x + 40 * i, y + 60 *i);
+                output = output.concat("?");
+
+            }
+        }
+    }
+}
+
