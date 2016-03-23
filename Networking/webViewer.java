2dea1acda9ff5a472ca2a256d812a93ff32fb9b7
jerryintoronto <jerry.kong@hotmail.ca>
2016-03-23 00:34:06 -0400

    simplest read from url

---
+++ Networking/webViewer.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.io.IOException;
+
+public class webViewer {
+    public static void main(String[] args) {
+
+        JFrame f = new JFrame();
+
+        try {
+            JEditorPane p = new JEditorPane("https://www.google.ca/");
+            p.setEditable(false);
+            f.add(new JScrollPane(p));
+        }
+
+        catch (IOException e) { }
+
+        f.setSize(1000, 800);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+
