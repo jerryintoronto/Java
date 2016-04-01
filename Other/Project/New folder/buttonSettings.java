863afd1bbb9c3bb356eee52427aca174ce2b1699
Jerry Kong <kongw@uwindsor.ca>
2016-04-01 12:55:06 -0400

    Update buttonSettings.java

--- Other/Project/New folder/buttonSettings.java
+++ Other/Project/New folder/buttonSettings.java
@@ -3,9 +3,6 @@ package finalProject;
 import javax.swing.*;
 import java.awt.*;
 
-/**
- * Created by Jerry on 3/28/2016.
- */
 public class buttonSettings {
     public buttonSettings(JButton a, JButton b, JButton c, JButton d, JButton e) {
         a.setFont(new Font("Arial", Font.BOLD, 20));
