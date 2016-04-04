8e81619969c9e7025f3146a7d1a8a9d0777f3e34
jerryintoronto <jerry.kong@hotmail.ca>
2016-04-04 12:30:48 -0400

    final project

---
+++ Other/60-280 Final Project/1.finished/Source Code/Point.java
@@ -0,0 +1,12 @@
+package finalProject;
+
+public class Point {
+
+    int x, y;
+
+    public Point(int a, int b) {
+        x = a;
+        y = b;
+
+    }
+}
---
+++ Other/60-280 Final Project/1.finished/Source Code/myFrame.java
@@ -0,0 +1,22 @@
+package finalProject;
+
+import javax.swing.*;
+
+public class myFrame extends JFrame {
+
+    public myFrame() {
+
+        super("starsClient");
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) { }
+
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1000, 1000);
+    }
+
+
+
+}
